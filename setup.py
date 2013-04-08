bea08c0619748759a72727421e2b1e3318e63214
Devon Jones <devon@knewton.com>
2013-04-08 10:01:02 -0600

    fixing a bunch of connection bugs.  Much more stable now.  Promoting to 0.9.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name="pirouette",
-	version="0.1.1",
+	version="0.9.0",
 	url = "https://github.com/devonjones/pirouette",
 	author="Devon Jones",
 	author_email="devon.jones@gmail.com",
